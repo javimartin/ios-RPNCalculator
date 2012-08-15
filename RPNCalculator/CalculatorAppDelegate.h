f83494c97833cbb3bd9eabfd4d37a754e0045951
Javier Martin <Javier@Javiers-MacBook-Pro.local>
2012-08-15 17:13:16 +0200

    Initial Commit

---
+++ RPNCalculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  RPNCalculator
+//
+//  Created by Javier Martin on 15/08/12.
+//  Copyright (c) 2012 Javier Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RPNCalculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  RPNCalculator
+//
+//  Created by Javier Martin on 15/08/12.
+//  Copyright (c) 2012 Javier Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
